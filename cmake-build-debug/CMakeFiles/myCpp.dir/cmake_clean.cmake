23ea92e8cf3464da942fba07545c01da66d901d7
StevenGeGe <Yong_GJ@163.com>
2019-03-19 22:35:55 +0800

    项目初始构建

---
+++ cmake-build-debug/CMakeFiles/myCpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myCpp.dir/main.cpp.obj"
+  "CMakeFiles/myCpp.dir/CMakeFiles/src/Student.cpp.obj"
+  "myCpp.pdb"
+  "myCpp.exe"
+  "myCpp.exe.manifest"
+  "libmyCpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "MYCPP_STUDENT.h"
+
+int main() {
+    using namespace std;
+    int num1,num2;
+    cout<<"输入两个数值：";
+    cin>>num1>>num2;
+    cout<<"num1:"<<num1<<endl;
+    cout<<"num2:"<<num2<<endl;
+
+
+
+    return 0;
+}
